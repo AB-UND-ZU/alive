c6c2d5990e4ba05e69026f4349b388a0f58d417c
Otto Sichert <git@ottosichert.de>
2023-03-17 16:19:32 +0100

    Add more entities

--- src/components/Terminal/index.js
+++ src/components/Terminal/index.js
@@ -7,31 +7,84 @@ const getDeterministicRandomInt = (minimum, maximum) => {
 };
 
 function Air() {
-  return <span className="Cell">&nbsp;</span>
+  return null;
 }
 
 function Player() {
-  return <span className="Cell Player">{'\u010b'}</span>
+  return (
+    <>
+      <span className="Entity Body">{'\u010b'}</span>
+      <span className="Entity Hair">~</span>
+    </>
+  )
 }
 
-function Object({ value }) {
-  const sizes = ['·', '∙', '\u0106', '\u0103'];
-  return <span className="Cell Object">{sizes[value]}</span>
+function Equipment({ type }) {
+  const types = {
+    Shield: '\u00ac',
+    Weapon: '/',
+  };
+  return <span className={`Entity Equipment ${type}`}>{types[type]}</span>
+}
+
+function Item({ size, type }) {
+  const sizes = {
+    small: '·',
+    medium: '∙',
+    large: '\u0106',
+    single: "'",
+    double: '"',
+    triple: '°',
+  };
+  return <span className={`Entity Item ${type}`}>{sizes[size]}</span>
+}
+
+function Chest() {
+  return (
+    <>
+      <span className="Entity Coin">{'\u011d'}</span>
+      <span className="Entity Chest">{'\u0115'}</span>
+      <span className="Entity Chest">{'\u011f'}</span>
+    </>
+  )
 }
 
 function Monster({ value }) {
   const sizes = ['\u011d', '\u010f', '\u011e', '\u0110'];
-  return <span className="Cell Monster">{sizes[value]}</span>
+  return <span className="Entity Monster">{sizes[value]}</span>
 }
 
-function Wall({ value }) {
-  const sizes = ['░', '▒', '▓', '█'];
-  return <span className="Cell Wall">{sizes[value]}</span>
+function Path() {
+  return <span className="Entity Path">{'░'}</span>
 }
 
-function Door({ value }) {
-  const sizes = ['\u0109', '\u0107', '█'];
-  return <span className="Cell Door">{sizes[value]}</span>
+function Water() {
+  return <span className="Entity Water">{'▓'}</span>
+}
+
+function Sand() {
+  return <span className="Entity Sand">{'▒'}</span>
+}
+
+function Wall() {
+  return <span className="Entity Wall">{'█'}</span>
+}
+
+function Plant({ value }) {
+  const sizes = ['\u03c4', '\u0104'];
+  return <span className="Entity Plant">{sizes[value]}</span>
+}
+
+function Lock() {
+  return <span className={`Entity Lock`}>{'\u011c'}</span>
+}
+
+function Door() {
+  return (
+    <>
+      <span className="Entity Door">{'\u0107'}</span>
+    </>
+  );
 }
 
 const cellTypes = [
@@ -40,21 +93,33 @@ const cellTypes = [
   () => <Air />,
   () => <Air />,
 
-  () => <Object value={0} />,
-  () => <Object value={1} />,
-  () => <Object value={2} />,
-  () => <Object value={3} />,
+  () => <Item size="tiny" type="Coin" />,
+  () => <Item size="small" type="Coin" />,
+  () => <Item size="medium" type="Coin" />,
+  () => <Item size="large" type="Coin" />,
 
-  () => <Door value={0} />,
-  () => <Door value={1} />,
-  () => <Door value={2} />,
+  () => <Door />,
+  () => [<Door />, <Lock />],
 
-  () => <Wall value={0} />,
-  () => <Wall value={1} />,
-  () => <Wall value={2} />,
-  () => <Wall value={3} />,
+  () => [<Item size="single" type="Drink" />, <Plant value={0} />],
+  () => [<Item size="double" type="Drink" />, <Plant value={0} />],
+  () => [<Item size="triple" type="Drink" />, <Plant value={0} />],
 
-  () => <Player />,
+  () => [<Item size="small" type="Food" />, <Plant value={1} />],
+  () => [<Item size="medium" type="Food" />, <Plant value={1} />],
+  () => [<Item size="large" type="Food" />, <Plant value={1} />],
+
+  () => <Wall />,
+
+  () => <Water />,
+  () => <Sand />,
+  () => <Path />,
+
+  () => [<Equipment type="Shield" />, <Player />, <Equipment type="Weapon" />],
+
+  () => [<Chest />],
+  () => [<Chest />, <Lock />],
+  () => [<Chest />, <Equipment type="Weapon" />],
 
   () => <Monster value={0} />,
   () => <Monster value={1} />,
@@ -64,11 +129,11 @@ const cellTypes = [
 
 function Terminal({ score, setScore, gameOver }) {
   const [board] = useState(() => {
-    const width = 33;
-    const height = 29;
+    const width = 19;
+    const height = 15;
     const rows = Array.from({ length: height }).map((_, rowIndex) => {
       const row = Array.from({ length: width }).map((_, columnIndex) => {
-        return cellTypes[getDeterministicRandomInt(0, cellTypes.length)]();
+        return cellTypes[getDeterministicRandomInt(1, cellTypes.length)]();
       });
       return row;
     });
@@ -89,7 +154,9 @@ function Terminal({ score, setScore, gameOver }) {
         <pre>
           {board.map(row => (
             <div className="Row">
-              {row}
+              {row.map(cells => (
+                <span className="Cell">{cells}</span>
+              ))}
             </div>
           ))}
         </pre>
