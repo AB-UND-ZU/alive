5596f2fd51b20b5c2807f91343aece0ce3630068
Otto Sichert <git@ottosichert.de>
2023-03-17 17:35:39 +0100

    Implement moving screen

--- src/components/Terminal/index.js
+++ src/components/Terminal/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 
 const getDeterministicRandomInt = (minimum, maximum) => {
   return Math.floor(
@@ -127,34 +127,71 @@ const cellTypes = [
   () => <Monster value={3} />,
 ]
 
+const sliceCenter = (array, index, width) => [...array, ...array, ...array].slice(array.length + index - (width - 1) / 2, array.length + index + (width + 1) / 2);
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'move': {
+      const { deltaX = 0, deltaY = 0 } = action;
+      return {
+        ...state,
+        x: (state.x + deltaX + state.width) % state.width,
+        y: (state.y + deltaY + state.height) % state.height,
+      };
+    }
+  }
+}
+
 function Terminal({ score, setScore, gameOver }) {
-  const [board] = useState(() => {
-    const width = 19;
-    const height = 15;
-    const rows = Array.from({ length: height }).map((_, rowIndex) => {
-      const row = Array.from({ length: width }).map((_, columnIndex) => {
+  const [state, dispatch] = useReducer(reducer, {
+    width: 300,
+    height: 250,
+    screenWidth: 19,
+    screenHeight: 15,
+    x: 150,
+    y: 100,
+  }, state => {
+    const rows = Array.from({ length: state.height }).map((_, rowIndex) => {
+      const row = Array.from({ length: state.width }).map((_, columnIndex) => {
         return cellTypes[getDeterministicRandomInt(1, cellTypes.length)]();
       });
       return row;
     });
-    return rows;
+    return { ...state, board: rows };
   });
 
+  useEffect(() => {
+    window.addEventListener('keydown', event => {
+      if (event.key === 'ArrowUp') {
+        dispatch({ type: 'move', deltaY: -1 });
+      } else if (event.key === 'ArrowRight') {
+        dispatch({ type: 'move', deltaX: 1 });
+      } else if (event.key === 'ArrowDown') {
+        dispatch({ type: 'move', deltaY: 1 });
+      } else if (event.key === 'ArrowLeft') {
+        dispatch({ type: 'move', deltaX: -1 });
+      } else {
+        return;
+      }
+      event.preventDefault();
+    });
+  }, []);
+
   return (
-    <div class="window">
-      <div class="title-bar">
-        <div class="title-bar-text">Alive.exe</div>
-        <div class="title-bar-controls">
+    <div className="window">
+      <div className="title-bar">
+        <div className="title-bar-text">Alive.exe</div>
+        <div className="title-bar-controls">
           <button aria-label="Minimize"></button>
           <button aria-label="Maximize"></button>
           <button aria-label="Close"></button>
         </div>
       </div>
-      <div class="window-body">
+      <div className="window-body">
         <pre>
-          {board.map(row => (
+          {sliceCenter(state.board, state.y, state.screenHeight).map(row => (
             <div className="Row">
-              {row.map(cells => (
+              {sliceCenter(row, state.x, state.screenWidth).map(cells => (
                 <span className="Cell">{cells}</span>
               ))}
             </div>
