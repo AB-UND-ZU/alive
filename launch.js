5e5c7346e1d34e14e3b4ef4803cff9851cd78c90
ottosichert <ottosichert@users.noreply.github.com>
2023-03-28 22:43:50 +0000

    deploy: aed3afa051c97bee83c46a337b0da580a37bfd28

--- launch.js
+++
@@ -1,4 +0,0 @@
-const { spawnSync } = require('child_process');
-
-// instead of launching React Dev Server, launch Rune pointing to React Dev Server
-spawnSync('npm', ['run', 'start:rune', '--', process.argv[2]], { stdio: 'inherit' });
