c67708f8d2d86bb22ddd4ee7693f3f87cd6f04b7
Otto Sichert <git@ottosichert.de>
2024-07-28 00:51:43 +0400

    Create fixed height screen

---
+++ src/components/Screen/index.tsx
@@ -0,0 +1,9 @@
+import "./index.css";
+
+export default function Screen() {
+  return (
+    <main className="Screen">
+      <span className="Dummy">Screen</span>
+    </main>
+  );
+}
---
+++ src/components/Stats/index.tsx
@@ -0,0 +1,5 @@
+import "./index.css";
+
+export default function Stats() {
+  return <header className="Stats">Stats</header>;
+}
