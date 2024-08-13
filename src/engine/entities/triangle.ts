dc2c99135b6828f05db17a71d86091ac79cd283d
Otto Sichert <git@ottosichert.de>
2024-08-13 17:14:33 +0400

    Place units above circular fog

--- src/engine/entities/triangle.ts
+++ src/engine/entities/triangle.ts
@@ -1,6 +1,8 @@
 import * as components from "../components";
 import { Collidable, COLLIDABLE } from "../components/collidable";
+import { FOG, Fog } from "../components/fog";
 import { Movable, MOVABLE } from "../components/movable";
+import { Npc, NPC } from "../components/npc";
 import { POSITION, Position } from "../components/position";
 import { Renderable, RENDERABLE } from "../components/renderable";
 import { SPRITE, Sprite } from "../components/sprite";
@@ -10,16 +12,20 @@ export default function createTriangle(
   world: World,
   entity: {
     [COLLIDABLE]: Collidable;
+    [FOG]: Fog;
+    [MOVABLE]: Movable;
+    [NPC]: Npc;
     [POSITION]: Position;
     [SPRITE]: Sprite;
     [RENDERABLE]: Renderable;
-    [MOVABLE]: Movable;
   }
 ) {
   const triangleEntity = world.createEntity();
 
   components.addCollidable(world, triangleEntity, entity[COLLIDABLE]);
+  components.addFog(world, triangleEntity , entity[FOG]);
   components.addMovable(world, triangleEntity, entity[MOVABLE]);
+  components.addNpc(world, triangleEntity , entity[NPC]);
   components.addPosition(world, triangleEntity, entity[POSITION]);
   components.addSprite(world, triangleEntity, entity[SPRITE]);
   components.addRenderable(world, triangleEntity, entity[RENDERABLE]);
