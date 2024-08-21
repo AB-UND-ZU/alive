c6170f0b16cb722a39d5a012344bb97fea5025e9
Otto Sichert <git@ottosichert.de>
2024-08-22 01:08:31 +0400

    Add orientable sprites

--- src/engine/components/sprite.ts
+++ src/engine/components/sprite.ts
@@ -1,17 +1,23 @@
 import { Entity } from "ecs";
 import { World } from "../ecs";
+import { Orientation } from "./movable";
 
 export type Layer = {
-  char: string,
-  color: string,
-}
+  char: string;
+  color: string;
+};
 export type Sprite = {
-  name: string,
-  layers: Layer[]
+  name: string;
+  layers: Layer[];
+  facing?: Record<Orientation, Layer[]>;
 };
 
 export const SPRITE = "SPRITE";
 
-export default function addSprite(world: World, entity: Entity, sprite: Sprite) {
+export default function addSprite(
+  world: World,
+  entity: Entity,
+  sprite: Sprite
+) {
   world.addComponentToEntity(entity, SPRITE, sprite);
 }
