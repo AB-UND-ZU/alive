be2f04ba4f9abf55bceb129033bdb6b1bbf8a273
Otto Sichert <git@ottosichert.de>
2024-11-15 22:19:25 +0100

    Render stacks in inventory as well

--- src/components/Row/index.tsx
+++ src/components/Row/index.tsx
@@ -1,7 +1,16 @@
+import { STACK_SIZE } from "../../engine/components/item";
 import { Layer, Sprite } from "../../engine/components/sprite";
 import "./index.css";
 
-function Cell({ layers }: { layers: Layer[] }) {
+export type CellSprite = Sprite & { stackableAmount?: number };
+
+function Cell({
+  layers,
+  stackableAmount,
+}: {
+  layers: Layer[];
+  stackableAmount?: number;
+}) {
   return (
     <div className="Cell">
       &nbsp;
@@ -10,15 +19,41 @@ function Cell({ layers }: { layers: Layer[] }) {
           {layer.char}
         </span>
       ))}
+      {stackableAmount && stackableAmount > 1 && (
+        <>
+          {Array.from({ length: stackableAmount }).map((_, index) => (
+            <span
+            key={index}
+              className="Dot"
+              style={{
+                top: `calc(var(--pixel-size) * (13 + ${
+                  index >= STACK_SIZE / 2 ? 2 : 0
+                }))`,
+                left: `calc(0.95 * var(--pixel-size) * ${
+                  (index % (STACK_SIZE / 2)) * 2
+                })`,
+              }}
+            ></span>
+          ))}
+        </>
+      )}
+      {/* 
+          position-x={((index % (STACK_SIZE / 2)) * 2 - 4) / pixels}
+          position-y={(-4.5 + (index >= STACK_SIZE / 2 ? -2 : 0)) / pixels}
+       */}
     </div>
   );
 }
 
-export default function Row({ cells = [] }: { cells?: Sprite[] }) {
+export default function Row({ cells = [] }: { cells?: CellSprite[] }) {
   return (
     <div className="Row">
       {cells.map((cell, index) => (
-        <Cell key={index} layers={cell.amounts?.multiple || cell.layers} />
+        <Cell
+          key={index}
+          layers={cell.amounts?.multiple || cell.layers}
+          stackableAmount={cell.stackableAmount}
+        />
       ))}
     </div>
   );
